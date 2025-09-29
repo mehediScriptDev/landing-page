b2391e3a74fa130de02ccef0659c6209ca015a14
Mehedi <mehedi25122003@gmail.com>
2025-09-29 12:53:05 +0600

    done but slider

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+
+
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
